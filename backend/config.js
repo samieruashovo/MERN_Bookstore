3b983721c2f1aa6fadbf5b3f6ed25df68e83490c
samieruashovo <samieruashovo@gmail.com>
2024-06-14 13:02:13 +0600

    first commit

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  "mongodb+srv://samier:samierdbpassword@cluster0.dljbowy.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Cluster0";
